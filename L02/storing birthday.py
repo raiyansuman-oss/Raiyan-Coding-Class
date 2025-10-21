98557834b334e1fd89419e857e2577fba0b49571
raiyansuman-oss <raiyansuman@gmail.com>
2025-10-21 20:43:27 +0600

    L03

---
+++ L02/storing birthday.py
@@ -0,0 +1,16 @@
+birthday1 = input("when is your birthday, Raheel? ")
+print()
+
+birthday2 = input ("when is your birthday, Raisat? ")
+print()
+
+birthday3 = input("when is your birthday, Nafi? ")
+print()
+
+birthday4 = input("when is your birthday, Rubina? ")
+print()
+
+birthday5 = input("when is your birthday, Monir? ")
+print()
+
+print("Raheel's birthday is "+birthday1)
---
+++ L03/L03 Typecasting.py
@@ -0,0 +1,31 @@
+# Assingning Different Variables
+name = "Penguin"
+age = 15
+weight = 38.5
+
+# Printing Diffrent Variables and their Data Type
+print("original:")
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print()
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print()
+
+print("Weight :", weight)
+print("Data Type of Weight is", type(weight))
+print()
+print()
+
+#Type casting to convert the datatype of variables
+print ("After Type Casting ....")
+
+age = str(age)
+print("age after typecasting: ",age)
+print("Data Type of age is",type(age))
+
+weight = int(weight)
+print("weight after typecasting: ",weight)
+print("Data Type of weight is",type(weight))
+
